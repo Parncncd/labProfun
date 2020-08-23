89e9ce120b67886c02e92b154388bdd5e6ef240f
Parncncd <69336494+Parncncd@users.noreply.github.com>
2020-08-23 13:45:29 +0700

    Add project files.

---
+++ labProfun/1.cpp
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main() {
+	int a, b, i;
+	printf("Enter first number : ");
+	scanf("%d", &a);
+	printf("Enter second number : ");
+	scanf("%d", &b);
+	if (a > b) {
+		i = b;
+	}
+	else if (b > a) {
+		i = a;
+	}
+	else {}
+	if (a != b) {
+		while (a % i != 0 || b % i != 0) {
+			i = i - 1;
+		}
+		printf("Greatest common divisor = %d", i);
+	}
+	else {
+		printf("Greatest common divisor = 1");
+	}
+
+
+
+
+	return 0;
+}
